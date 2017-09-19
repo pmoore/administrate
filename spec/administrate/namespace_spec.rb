2ba845e9a4dcd796371e449895285560896b0858
pmoore <pmmoor1@gmail.com>
2017-09-19 17:08:16 -0400

    Adding tests for namespace update, checking "/" in namespace.

--- spec/administrate/namespace_spec.rb
+++ spec/administrate/namespace_spec.rb
@@ -3,15 +3,27 @@ require "administrate/namespace"
 
 describe Administrate::Namespace do
   describe "#resources" do
+    let(:namespace) { Administrate::Namespace.new(:admin) }
+
     it "searches the routes for resources in the namespace" do
       begin
-        namespace = Administrate::Namespace.new(:admin)
-
         Rails.application.routes.draw do
           namespace(:admin) { resources :customers }
         end
 
         expect(namespace.resources.map(&:to_sym)).to eq [:customers]
+      ensure
+        reset_routes
+      end
+    end
+
+    it "does not include resources with prefix that equals namespace" do
+      begin
+        Rails.application.routes.draw do
+          resources :admin_users
+        end
+
+        expect(namespace.resources.map(&:to_sym)).to eq []
       ensure
         reset_routes
       end
